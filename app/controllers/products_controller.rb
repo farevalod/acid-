b7bd28a8afacebb26f6321c0faca5874e43961dc
Francisco Arevalo <farevalod@gmail.com>
2016-10-05 01:06:47 -0300

    Fix indentation

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,107 +1,107 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: [:show, :edit, :update, :destroy, :add, :remove]
+	before_action :set_product, only: [:show, :edit, :update, :destroy, :add, :remove]
 
-  # GET /products
-  # GET /products.json
-  def index
-    @products = Product.all
-	if not current_user.nil?
-		@cart = current_user.cart
-	else
-		new_user = User.new
-		new_user.email="guest"+rand(1024).to_s+"@ex.com"
-		new_user.password = "123456"
-		new_user.save
-		sign_in(new_user)
-		@cart = Cart.new
-		new_user.carts << @cart
-		@cart.save
+	# GET /products
+	# GET /products.json
+	def index
+		@products = Product.all
+		if not current_user.nil?
+			@cart = current_user.cart
+		else
+			new_user = User.new
+			new_user.email="guest"+rand(1024).to_s+"@ex.com"
+			new_user.password = "123456"
+			new_user.save
+			sign_in(new_user)
+			@cart = Cart.new
+			new_user.carts << @cart
+			@cart.save
+		end
 	end
-  end
 
-  # POST /products/1/add
-  # POST /products/1/add.json
-  def add
-	@cart = current_user.carts.last
-	@cart.products << @product
-	redirect_to products_url
-  end
+	# POST /products/1/add
+	# POST /products/1/add.json
+	def add
+		@cart = current_user.carts.last
+		@cart.products << @product
+		redirect_to products_url
+	end
 
-  def remove
-	@cart = current_user.carts.last
-	@cart.products.delete(@product)
-	redirect_to products_url
-  end
-#
-  # GET /products/1
-  # GET /products/1.json
-  def show
-  end
+	def remove
+		@cart = current_user.carts.last
+		@cart.products.delete(@product)
+		redirect_to products_url
+	end
+	#
+	# GET /products/1
+	# GET /products/1.json
+	def show
+	end
 
-  # GET /products/new
-  def new
-    @product = Product.new
-  end
+	# GET /products/new
+	def new
+		@product = Product.new
+	end
 
-  # GET /products/1/edit
-  def edit
-	  @categories = Category.all
-  end
+	# GET /products/1/edit
+	def edit
+		@categories = Category.all
+	end
 
-  # POST /products
-  # POST /products.json
-  def create
-    @product = Product.new(product_params)
+	# POST /products
+	# POST /products.json
+	def create
+		@product = Product.new(product_params)
 
-    respond_to do |format|
-      if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
-        format.json { render :show, status: :created, location: @product }
-      else
-        format.html { render :new }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+		respond_to do |format|
+			if @product.save
+				format.html { redirect_to @product, notice: 'Product was successfully created.' }
+				format.json { render :show, status: :created, location: @product }
+			else
+				format.html { render :new }
+				format.json { render json: @product.errors, status: :unprocessable_entity }
+			end
+		end
+	end
 
-  # PATCH/PUT /products/1
-  # PATCH/PUT /products/1.json
-  def update
-    respond_to do |format|
-	  p product_params
-      if @product.update(product_params)
-        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
-        format.json { render :show, status: :ok, location: @product }
-      else
-        format.html { render :edit }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+	# PATCH/PUT /products/1
+	# PATCH/PUT /products/1.json
+	def update
+		respond_to do |format|
+			p product_params
+			if @product.update(product_params)
+				format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+				format.json { render :show, status: :ok, location: @product }
+			else
+				format.html { render :edit }
+				format.json { render json: @product.errors, status: :unprocessable_entity }
+			end
+		end
+	end
 
-  def import
-	Product.import(params[:file])
-	redirect_to root_url, notice: "Productos importados."
-  end
+	def import
+		Product.import(params[:file])
+		redirect_to root_url, notice: "Productos importados."
+	end
 
-  # DELETE /products/1
-  # DELETE /products/1.json
-  def destroy
-    @product.destroy
-    respond_to do |format|
-      format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+	# DELETE /products/1
+	# DELETE /products/1.json
+	def destroy
+		@product.destroy
+		respond_to do |format|
+			format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
+			format.json { head :no_content }
+		end
+	end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_product
-      @product = Product.find(params[:id])
-    end
+	private
+	# Use callbacks to share common setup or constraints between actions.
+	def set_product
+		@product = Product.find(params[:id])
+	end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def product_params
-      params.require(:product).permit(:name, :price, :description, category_ids: [])
-    end
+	# Never trust parameters from the scary internet, only allow the white list through.
+	def product_params
+		params.require(:product).permit(:name, :price, :description, category_ids: [])
+	end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,25 +4,24 @@ class Product < ApplicationRecord
 	accepts_nested_attributes_for :categories
 
 	def self.import(file)
-	  spreadsheet = open_spreadsheet(file)
-	  header = spreadsheet.row(1)
-	  (2..spreadsheet.last_row).each do |i|
-		row = Hash[[header, spreadsheet.row(i)].transpose]
-		product = find_by_id(row["id"]) || new
-		product.attributes = row.to_hash
-		if (not product.name.blank?) and (not product.price.blank?)
-			product.save!
+		spreadsheet = open_spreadsheet(file)
+		header = spreadsheet.row(1)
+		(2..spreadsheet.last_row).each do |i|
+			row = Hash[[header, spreadsheet.row(i)].transpose]
+			product = find_by_id(row["id"]) || new
+			product.attributes = row.to_hash
+			if (not product.name.blank?) and (not product.price.blank?)
+				product.save!
+			end
 		end
-	  end
 	end
 
 	def self.open_spreadsheet(file)
-	  case File.extname(file.original_filename)
-		  when ".csv" then Roo::CSV.new(file.path, csv_options: {encoding: "iso-8859-1:utf-8"})
-		  when ".xls" then Roo::Excel.new(file.path)
-		  when ".xlsx" then Roo::Excelx.new(file.path)
-		  else raise "Archivo invalido: #{file.original_filename}"
-	  end
+		case File.extname(file.original_filename)
+		when ".csv" then Roo::CSV.new(file.path, csv_options: {encoding: "iso-8859-1:utf-8"})
+		when ".xls" then Roo::Excel.new(file.path)
+		when ".xlsx" then Roo::Excelx.new(file.path)
+		else raise "Archivo invalido: #{file.original_filename}"
+		end
 	end
-
 end
