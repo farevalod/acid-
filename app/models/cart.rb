47e1af105977275f05f0cf925c3e5ed691fc50f0
Francisco Arevalo <farevalod@gmail.com>
2016-10-10 20:56:25 -0300

    cart_total usando sum

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,10 +2,6 @@ class Cart < ApplicationRecord
 	belongs_to :user
 	has_and_belongs_to_many :products
 	def cart_total
-		total = 0
-		products.each do |p|
-			total += p.price
-		end
-		total
+		products.sum{|p| p.price}
 	end
 end
